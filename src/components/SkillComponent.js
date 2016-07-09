52ea308da31832c68dfe48767e5b765a57c55b8d
Barrett <bwolfson@pergetown.com>
2016-07-09 19:34:31 -0400

    change skill message

--- src/components/SkillComponent.js
+++ src/components/SkillComponent.js
@@ -6,6 +6,7 @@ export default class SkillComponent extends React.Component {
 
     return (
       <div class="col-md-4">
+      	<h1>mad skills</h1>
         <h4>{title}</h4>
         <p>description about a skill</p>
       </div>
